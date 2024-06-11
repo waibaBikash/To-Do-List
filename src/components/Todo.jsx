9598b4ce77ffe34f59f162d6ecd95aa4d83ae99b
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 17:25:35 +1000

    create a add

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,7 +12,7 @@ const Todo = () => {
         {/* ########## input box ######## */}
          <div className='flex items-center my-7 bg-gray-200 rounded-full'>
             <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-            <button>Add +</button>
+            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
          </div>
     </div>
   )
