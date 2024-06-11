8a3f71dd38bdaac3876bd0357620adc8f7209fb5
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 17:35:33 +1000

    fix text

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -6,9 +6,9 @@ import delete_icon from '../assets/delete.png';
 const TodoItems = () => {
   return (
     <div className='flex items-center my-3 gap-2'>
-       <div>
-          <img src={tick} alt="" />
-          <p>Learn Coding</p>
+       <div className='flex flex-1 items-center cursor-pointer'>
+          <img src={tick} alt="" className='w-7' />
+          <p className='text-slate-700 ml-4 text-[17px]'>Learn Coding</p>
        </div>
     </div>
   )
