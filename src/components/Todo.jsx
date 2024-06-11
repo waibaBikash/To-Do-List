4fedbdf9a701a03e2d898aca6063398c6f96dbcb
waibaBikash <kavrelibikash@gmail.com>
2024-06-11 17:32:37 +1000

    add list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import todo_icon from '../assets/todo_icon.png';
+import TodoItems from './TodoItems';
 
 const Todo = () => {
   return (
@@ -14,6 +15,12 @@ const Todo = () => {
             <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
             <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
          </div>
+         {/* ######### Todo List ############ */}
+         
+          <div className=''>
+              <TodoItems />
+          </div>
+
     </div>
   )
 }
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import tick from '../assets/tick.png';
+import not_tick from '../assets/not_tick.png';
+import delete_icon from '../assets/delete.png';
 
 const TodoItems = () => {
   return (
-    <div>
-
+    <div className='flex items-center my-3 gap-2'>
+       <div>
+          <img src={tick} alt="" />
+          <p>Learn Coding</p>
+       </div>
     </div>
   )
 }
