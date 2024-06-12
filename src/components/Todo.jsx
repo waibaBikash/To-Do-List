1d8b76f875f3f27dbea41c35de76208e01bb659f
waibaBikash <kavrelibikash@gmail.com>
2024-06-12 11:34:15 +1000

    use of trim to avoid extra space

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import todo_icon from '../assets/todo_icon.png';
 import TodoItems from './TodoItems';
 
 const Todo = () => {
+  const inputRef = useRef();
+
+  const add =() => {
+      const inputText = inputRef.current.value.trim();
+      console.log(inputText);
+  }
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
          {/* ####### title ########### */}
@@ -12,8 +18,8 @@ const Todo = () => {
         </div>
         {/* ########## input box ######## */}
          <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-            <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
-            <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
+            <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task' />
+            <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
          </div>
          {/* ######### Todo List ############ */}
          
